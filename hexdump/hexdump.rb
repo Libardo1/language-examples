ab8cd982586d264524ec6de955fafe50421dc1e0
mkfs <semper.non.sequitur@gmail.com>
2011-07-08 21:55:01 -0400

    LANG : Made ruby hexdump a bit more re-usable

--- hexdump/hexdump.rb
+++ hexdump/hexdump.rb
@@ -1,10 +1,24 @@
 #!/usr/bin/env ruby
+# A standard hexdump program. Outputs 16 bytes per line, in hex and ASCII.
 
-offset = 0
-while (buf = ARGF.read(16)) do
-       bytes = buf.unpack 'C*'
-       puts "%08X :%-48s |%-16s|" % [ offset, 
-              bytes.map{ |b| " %02X" % b },
-              bytes.map{ |b| b.chr =~ /[[:print:]]/ ? b.chr : '.' }.join('') ]
-       offset += 16
+=begin rdoc
+Given a file handle, return an array of lines for the hexdump of that file.
+=end
+def hexdump(f)
+  lines = []
+  offset = 0
+  while (buf = f.read(16)) do
+    bytes = buf.unpack 'C*'
+    lines << "%08X :%-48s |%-16s|" % [ 
+                 offset, 
+                 bytes.map { |b| " %02X" % b },
+                 bytes.map { |b| b.chr =~ /[[:print:]]/ ? b.chr : '.' }.join('') 
+             ]
+    offset += 16
+  end
+  lines
+end
+
+if __FILE__ == $0
+  hexdump(ARGF).each { |line| puts line }
 end
